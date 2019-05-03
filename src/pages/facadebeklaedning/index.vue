efaf941bd1ed8c7bac57f99a86a2f855905a490a
fmeyer1980 <fm@erhvervswebdesign.dk>
2019-05-03 17:55:34 +0200

    billede

--- src/pages/facadebeklaedning/index.vue
+++ src/pages/facadebeklaedning/index.vue
@@ -2,6 +2,8 @@
   <Layout>
     <h1>Fasacebeklædning</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <!-- <g-image class="hero__bg-image" src="~/assets/images/design.jpg" width="1600" quality="100" /> -->
+    <img src="@/assets/images/design.jpg" alt="">
   </Layout>
 </template>
 
@@ -12,3 +14,11 @@ export default {
   }
 }
 </script>
+<style scoped>
+  img{
+    max-width: 100%;
+    height: auto;
+    margin: auto;
+  }
+</style>
+
