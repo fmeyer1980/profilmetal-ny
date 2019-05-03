3031217f20bac81cf071988669494b92b023c546
fmeyer1980 <fm@erhvervswebdesign.dk>
2019-05-03 17:14:47 +0200

    header i default fil

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -26,3 +26,8 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+
+</style>
+
--- src/pages/facadebeklaedning/index.vue
+++ src/pages/facadebeklaedning/index.vue
@@ -8,7 +8,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'About me'
+    title: 'Fasacebeklædning'
   }
 }
 </script>
